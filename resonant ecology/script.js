c3d87bac7604704672fae8a0d1c3cc1e3b172cb3
NoaLwx <130018250+NoaLwx@users.noreply.github.com>
2025-03-12 19:25:51 +1100

    aaaa

---
+++ resonant ecology/script.js
@@ -0,0 +1,8 @@
+const dropdownmenu = document.querySelector(".menu");
+const sidemenu = document.querySelector("#side-menu");
+
+dropdownmenu.addEventListener("click", function(){
+    // sidemenu.classList.toggle("show");
+    console.log("click");
+    
+});
--- resonant ecology/scripts.js
+++
@@ -1,5 +0,0 @@
-const dropdownmenu = document.querySelector(".menu");
-
-dropdownmenu.addEventListener("click", function(){
-    dropdownmenu.classList.add("show");
-});
